68724a9cd2a111432054318a4cca33af4488987b
Mark Simpson <mark.simpson78@hotmail.co.uk>
2015-01-09 20:53:13 +0000

    Add Prototype stuff

---
+++ sites/all/themes/marky_v4/prototype/stream.php
@@ -0,0 +1,100 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<table width="100%" border="0" cellspacing="3" cellpadding="3">
+  <tbody><tr>
+    <td class="p"><span class="big_price">LATEST NEWS</span></td>
+  </tr>
+  <tr>
+    <td>      <table width="98%" height="62" border="0" cellpadding="0" cellspacing="0">
+      
+        
+        <tbody><tr>
+          <td height="55" valign="top" class="big_price2"><table width="100%" border="0" cellpadding="0" cellspacing="0" class="p">
+              <tbody><tr>
+                <td width="5%" rowspan="3" align="center"><a href="blog.php?ID=23" alt="Mark Simpson" style="cursor:pointer;"><img src="images/mark.jpg" alt="Mark Simpson" width="45" border="0" class="corner iradius10 ishadow33"></a></td>
+                <td width="1%" rowspan="3">&nbsp;</td>
+                <td width="94%" valign="top"><span class="Lesser_header2">Xmas</span></td>
+              </tr>
+              <tr>
+                <td valign="top"><span class="big_price2bold"><span class="small_include_boxes">New project on the horizon, collaboration where developmen..</span></span></td>
+              </tr>
+              <tr>
+                <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody><tr>
+                      <td width="57%"><span class="big_price2bold">Date Posted: <span class="tooled">16:02 - 22nd November</span></span></td>
+                      <td width="2%">&nbsp;</td>
+                      <td width="41%" align="right" valign="top"><a href="blog.php?ID=23" class="tooledlink"><img src="images/view.png" alt="View Full Blog" border="0"></a></td>
+                    </tr>
+                    
+                </tbody></table></td>
+              </tr>
+          </tbody></table></td>
+        </tr>
+      </tbody></table>
+          <table width="98%" height="62" border="0" cellpadding="0" cellspacing="0">
+      
+        
+        <tbody><tr>
+          <td height="55" valign="top" class="big_price2"><table width="100%" border="0" cellpadding="0" cellspacing="0" class="p">
+              <tbody><tr>
+                <td width="5%" rowspan="3" align="center"><a href="blog.php?ID=19" alt="Mark Simpson" style="cursor:pointer;"><img src="images/mark.jpg" alt="Mark Simpson" width="45" border="0" class="corner iradius10 ishadow33"></a></td>
+                <td width="1%" rowspan="3">&nbsp;</td>
+                <td width="94%" valign="top"><span class="Lesser_header2">Turning Point - newsletter and microsite</span></td>
+              </tr>
+              <tr>
+                <td valign="top"><span class="big_price2bold"><span class="small_include_boxes">Currently creating a mass emailer to relay a weekly newsle..</span></span></td>
+              </tr>
+              <tr>
+                <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody><tr>
+                      <td width="57%"><span class="big_price2bold">Date Posted: <span class="tooled">16:27 - 26th October</span></span></td>
+                      <td width="2%">&nbsp;</td>
+                      <td width="41%" align="right" valign="top"><a href="blog.php?ID=19" class="tooledlink"><img src="images/view.png" alt="View Full Blog" border="0"></a></td>
+                    </tr>
+                    
+                </tbody></table></td>
+              </tr>
+          </tbody></table></td>
+        </tr>
+      </tbody></table>
+          <table width="98%" height="62" border="0" cellpadding="0" cellspacing="0">
+      
+        
+        <tbody><tr>
+          <td height="55" valign="top" class="big_price2"><table width="100%" border="0" cellpadding="0" cellspacing="0" class="p">
+              <tbody><tr>
+                <td width="5%" rowspan="3" align="center"><a href="blog.php?ID=22" alt="Mark Simpson" style="cursor:pointer;"><img src="images/mark.jpg" alt="Mark Simpson" width="45" border="0" class="corner iradius10 ishadow33"></a></td>
+                <td width="1%" rowspan="3">&nbsp;</td>
+                <td width="94%" valign="top"><span class="Lesser_header2">Flash skills wavering</span></td>
+              </tr>
+              <tr>
+                <td valign="top"><span class="big_price2bold"><span class="small_include_boxes">Why use flash when Jquery creates such lovely animation?
+..</span></span></td>
+              </tr>
+              <tr>
+                <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody><tr>
+                      <td width="57%"><span class="big_price2bold">Date Posted: <span class="tooled">13:45 - 2nd October</span></span></td>
+                      <td width="2%">&nbsp;</td>
+                      <td width="41%" align="right" valign="top"><a href="blog.php?ID=22" class="tooledlink"><img src="images/view.png" alt="View Full Blog" border="0"></a></td>
+                    </tr>
+                    
+                </tbody></table></td>
+              </tr>
+          </tbody></table></td>
+        </tr>
+      </tbody></table>
+        <table width="98%" border="0" cellspacing="0" cellpadding="0">
+      <tbody><tr>
+        <td align="right"><a href="http://www.marksimpson.me.uk/rss_1.php" target="_blank"><img src="images/rss16.png" alt="Subscribe to RSS Web Feed" width="16" height="16" border="0"></a></td>
+      </tr>
+    </tbody></table></td>
+  </tr>
+</tbody></table>
